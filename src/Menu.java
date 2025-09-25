66ad044b5ec9ce46fed39a5d083f6257b29f83d4
Simon Toivola <simontoivola@gmail.com>
2025-09-25 13:24:40 +0200

    isInputValid added

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+
+
+import static java.io.IO.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu {
+
+    public static char userChoice;
+
+    public static boolean is_input_valid(char input) {
+
+        String validInput = "+-*/%x";
+
+        String inputAsString = String.valueOf( input );
+
+        return validInput.contains( inputAsString );
+    }
 }
