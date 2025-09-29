0dbb4363aba55de35f9f99a4b1c678213eb8bc6b
Simon Toivola <simontoivola@gmail.com>
2025-09-29 14:11:08 +0200

    adding list of valid operators to help

--- src/Help.java
+++ src/Help.java
@@ -5,10 +5,20 @@ public class Help {
 
     public static void getHelp() {
 
-        println("När du väljer i menyn skriv in ETT tecken");
-        println("När du skriver in talen separera de med mellanslag. Vill du ha negativa tal separera inte minustecknet och talet");
-        println("Flyttal ska skrivas med punk ");
-        println("Exponentiering ska endast ha positiva operander");
-        println("");
+        println("--------------------------------------------------------------");
+        println("När du väljer i menyn skriv in ETT tecken.");
+        println("När du skriver in talen separera de med mellanslag.");
+        println("Vill du ha negativa tal separera inte minustecknet och talet.");
+        println("Flyttal ska skrivas med punkt.");
+        println("Exponentiering ska endast ha positiva operander.\n");
+        println("Kalkylatorn stöder dessa operatorer");
+        println("+: addition");
+        println("-: subtraktion");
+        println("*: multiplikation");
+        println("/: division");
+        println("%: modulos");
+        println("^: exponentiering");
+        println("--------------------------------------------------------------\n");
+
     }
 }
